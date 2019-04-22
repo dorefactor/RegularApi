11a522c6db1b617a9c30c8542f3ed142f4753ca9
Francisco Castellanos <fvcastellanos@gmail.com>
2019-04-21 20:54:16 -0600

    [travis support]: configure listening ports on rabbit container

--- Scripts/bash/prepare-build.sh
+++ Scripts/bash/prepare-build.sh
@@ -3,4 +3,4 @@
 echo "Run configuration or migration scripts"
 
 docker pull rabbitmq:3.7
-docker run -d --hostname rabbitmq-host --name rabbitmq rabbitmq:3.7
+docker run -d -p 0.0.0.0:5672:5672 --hostname rabbitmq-host --name rabbitmq rabbitmq:3.7
