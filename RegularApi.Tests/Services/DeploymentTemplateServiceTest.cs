4c2f985193f33fb8db78f2b2ce016c47a2d141f9
Francisco Castellanos <fvcastellanos@gmail.com>
2019-04-29 14:27:00 -0600

    [deployment template]: add deployment template service tests

--- RegularApi.Tests/Services/DeploymentTemplateServiceTest.cs
+++ RegularApi.Tests/Services/DeploymentTemplateServiceTest.cs
@@ -63,6 +63,87 @@ namespace RegularApi.Tests.Services
             _deploymentTemplateDaoMock.Verify(dao => dao.GetByNameAsync(TemplateName));
             _deploymentTemplateDaoMock.VerifyNoOtherCalls();
         }
-        
+
+        [Test]
+        public async Task GetDeploymentTemplateThrowsExceptionTest()
+        {
+            _deploymentTemplateDaoMock.Setup(dao => dao.GetByNameAsync(TemplateName))
+                .Throws<Exception>();
+
+            var result = await _service.GetDeploymentTemplateByNameAsync(TemplateName);
+
+            Assert.True(result.IsLeft);
+            var expectedError = result.LeftAsEnumerable().First();
+
+            expectedError.Should().Be("Can't get deployment template: " + TemplateName);
+
+            _deploymentTemplateDaoMock.Verify(dao => dao.GetByNameAsync(TemplateName));
+            _deploymentTemplateDaoMock.VerifyNoOtherCalls();            
+        }
+
+        [Test]
+        public async Task NewDeploymentTemplateTest()
+        {
+            var template = ModelFactory.BuildDeploymentTemplate(TemplateName);
+
+            _deploymentTemplateDaoMock.Setup(dao => dao.GetByNameAsync(TemplateName))
+                .ReturnsAsync(Option<DeploymentTemplate>.None);
+
+            _deploymentTemplateDaoMock.Setup(dao => dao.NewAsync(template))
+                .ReturnsAsync(template);
+            
+            var result = await _service.AddDeploymentTemplateAsync(template);
+            
+            Assert.True(result.IsRight);
+            var expectedTemplate = result.RightAsEnumerable().First();
+            
+            expectedTemplate.Should().BeEquivalentTo(template);
+
+            _deploymentTemplateDaoMock.Verify(dao => dao.GetByNameAsync(TemplateName));
+            _deploymentTemplateDaoMock.Verify(dao => dao.NewAsync(template));
+            _deploymentTemplateDaoMock.VerifyNoOtherCalls();            
+        }
+
+        [Test]
+        public async Task AddExistingDeploymentTemplateTest()
+        {
+            var template = ModelFactory.BuildDeploymentTemplate(TemplateName);
+
+            _deploymentTemplateDaoMock.Setup(dao => dao.GetByNameAsync(TemplateName))
+                .ReturnsAsync(Option<DeploymentTemplate>.Some(template));
+            
+            var result = await _service.AddDeploymentTemplateAsync(template);
+            
+            Assert.True(result.IsLeft);
+            var expectedError = result.LeftAsEnumerable().First();
+            
+            expectedError.Should().BeEquivalentTo("Deployment template: " + TemplateName + " already exists");
+
+            _deploymentTemplateDaoMock.Verify(dao => dao.GetByNameAsync(TemplateName));
+            _deploymentTemplateDaoMock.VerifyNoOtherCalls();                        
+        }
+
+        [Test]
+        public async Task AddDeploymentTemplateThrowsExceptionTest()
+        {
+            var template = ModelFactory.BuildDeploymentTemplate(TemplateName);
+
+            _deploymentTemplateDaoMock.Setup(dao => dao.GetByNameAsync(TemplateName))
+                .ReturnsAsync(Option<DeploymentTemplate>.None);
+
+            _deploymentTemplateDaoMock.Setup(dao => dao.NewAsync(template))
+                .Throws<Exception>();
+            
+            var result = await _service.AddDeploymentTemplateAsync(template);
+            
+            Assert.True(result.IsLeft);
+            var expectedError = result.LeftAsEnumerable().First();
+            
+            expectedError.Should().BeEquivalentTo("Can't create deployment template: " + TemplateName);
+
+            _deploymentTemplateDaoMock.Verify(dao => dao.GetByNameAsync(TemplateName));
+            _deploymentTemplateDaoMock.Verify(dao => dao.NewAsync(template));
+            _deploymentTemplateDaoMock.VerifyNoOtherCalls();                        
+        }
     }
 }
