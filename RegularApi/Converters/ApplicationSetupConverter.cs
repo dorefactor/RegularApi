f850a31523340be3b9c40a65ed29acae1290adc4
Jorge Alvarez <alvarez.jeap@gmail.com>
2019-05-10 09:36:44 -0600

    Add missed tests

--- RegularApi/Converters/ApplicationSetupConverter.cs
+++ RegularApi/Converters/ApplicationSetupConverter.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using RegularApi.Domain.Views;
 using RegularApi.Domain.Views.Docker;
+using RegularApi.Enums;
 
 namespace RegularApi.Converters
 {
@@ -17,22 +18,26 @@ namespace RegularApi.Converters
         }
 
         public override void WriteJson(JsonWriter writer,
-                 object value, JsonSerializer serializer)
+                                       object value, 
+                                       JsonSerializer serializer)
         {
             throw new InvalidOperationException("Use default serialization.");
         }
 
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        public override object ReadJson(JsonReader reader, 
+                                        Type objectType, 
+                                        object existingValue, 
+                                        JsonSerializer serializer)
         {
 
             var jsonObject = JObject.Load(reader);
             var applicationSetup = default(ApplicationSetupView);
             var applicationType = jsonObject.Value<string>("type");
-            var applicationTypeEnum = (Enums.ApplicationType)Enum.Parse(typeof(Enums.ApplicationType), applicationType);
+            var applicationTypeEnum = (ApplicationType)Enum.Parse(typeof(ApplicationType), applicationType);
 
             switch (applicationTypeEnum)
             {
-                case Enums.ApplicationType.Docker:
+                case ApplicationType.Docker:
                     applicationSetup = new DockerApplicationSetupView();
                     break;
             }
