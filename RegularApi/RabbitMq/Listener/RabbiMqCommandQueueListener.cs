f634d61d7bad91f1ecafcf4c41d2cdd8bb31f10b
Francisco Castellanos <fvcastellanos@gmail.com>
2019-06-21 14:56:22 -0600

    [configure docker]: add drone api call for deployment order

--- RegularApi/RabbitMq/Listener/RabbiMqCommandQueueListener.cs
+++ RegularApi/RabbitMq/Listener/RabbiMqCommandQueueListener.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using RabbitMQ.Client;
+using RegularApi.Domain.Views.Drone;
 
 namespace RegularApi.RabbitMq.Listener
 {
@@ -18,7 +21,8 @@ namespace RegularApi.RabbitMq.Listener
         {
             _logger = logger;
             _channel = CreateConnection(connectionFactory);
-            _httpClient = httpClientFactory.CreateWithBasicAuth();
+
+            _httpClient = httpClientFactory.CreateWithBearerToken();
 
             AddQueueListener(_channel, queue);
         }
@@ -27,7 +31,22 @@ namespace RegularApi.RabbitMq.Listener
         {
             _logger.LogInformation("message received: {0}", message);
 
-            await _httpClient.PostAsync("regular-deployer/job/deployer/buildWithParameters?DEPLOYMENT_ORDER_ID=" + message, null);
+            var buildsRequest = await _httpClient.GetAsync("api/repos/dorefactor/rd-jenkins-deployer/builds");
+
+            if (buildsRequest.IsSuccessStatusCode)
+            {
+                var content = await buildsRequest.Content.ReadAsStringAsync();
+                var buildList = JsonConvert.DeserializeObject<List<BuildView>>(content);
+                var count = buildList.Count;
+
+                await _httpClient.PostAsync("api/repos/dorefactor/rd-jenkins-deployer/builds/" + count + "?DEPLOYMENT_ORDER_ID=" + message, null);
+
+                return;
+            }
+
+            _logger.LogError("can't get last deployment number for order: {0}", message);
+
+            // await _httpClient.PostAsync("regular-deployer/job/deployer/buildWithParameters?DEPLOYMENT_ORDER_ID=" + message, null);
         }
     }
 }
